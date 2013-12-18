40e14627ae7fb9ae06a8d6a6560226c0d6097566
Igor C. Batista <igor.batista@gmail.com>
2013-12-18 13:12:16 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Emprestimo::Application.config.secret_key_base = 'c68c946290ea3154421ffa5e85c7ac39349e275900d90cc93e58d88c96a2829773bac05e94998e6b8cea02a6bb33b22f660df87ea8809f8c7a7f7a49b9e676e8'
