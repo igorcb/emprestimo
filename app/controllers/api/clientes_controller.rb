b4f0b114d4f64ee9f5ebc9a29abd19efac7abf8d
Igor Batista <igor.batista@gmail.com>
2014-03-31 22:14:15 -0300

    api update client

--- app/controllers/api/clientes_controller.rb
+++ app/controllers/api/clientes_controller.rb
@@ -34,6 +34,24 @@ class Api::ClientesController < ApiController
     end
   end  
 
+  def update
+    begin
+    @cliente = Cliente.find_by_empresa_id_and_uuid(params[:cliente][:empresa_id],params[:cliente][:uuid])
+    empresa_id = params[:cliente][:empresa_id]
+    uuid_id = params[:cliente][:uuid]
+    if @cliente.update_attributes(params[:cliente])
+      render(notice: 'Cliente was successfully updated.');
+    else
+      @errors = { :errors => @cliente.errors.each {|m| m.to_s} }
+      render json: @errors.to_json, status: 200
+    end
+    rescue Exception => e
+      @errors = { :errors => 'update cliente, empresa_id: #{empresa_id} and uuid: #{uuid_id} record not found.' }
+      render json: @errors.to_json, :status => 200
+    end
+
+  end  
+
   def clientes_uuid
     @clientes = Cliente.where(uuid: [params[:cliente]])
     respond_to do |format|
---
+++ app/views/api/clientes/update.json.rabl
@@ -0,0 +1,3 @@
+object @cliente
+
+extends 'api/clientes/show'
